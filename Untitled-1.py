cf21bb910d778e553b6582266bf79edeb8b88bc9
dudalmeida18 <167311930+dudalmeida18@users.noreply.github.com>
2024-05-22 10:08:04 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,5 @@
+area = float(input("digite a area do quadrado: "))
+areatotal = area * area
+print (f"a área do quadrado e:{areatotal}")
+areadobro = areatotal*2
+print (f"o dobro dessa área e:{areadobro}")
