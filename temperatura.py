a70ed40013a5b90c3846f4ea8cf665af14ddfd50
dudalmeida18 <167311930+dudalmeida18@users.noreply.github.com>
2024-05-22 10:57:28 -0300

    Add files via upload

---
+++ temperatura.py
@@ -0,0 +1,4 @@
+salario = float(input("quanto você ganha por hora? "))
+hora = float(input("quantas horas você trabalha por mês"))
+salariodomes = salario*hora
+print(f"a tatal do seu salario no mês e:{salariodomes}")
